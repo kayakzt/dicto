0f6fe651b255e858d5825064756798c96c24a69e
kaya <6240107+kayakzt@users.noreply.github.com>
2026-02-05 00:30:28 +0900

    refactor: modernize cli and weblio fetch

--- main.go
+++ main.go
@@ -1,31 +1,48 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
-var (
-	eiji bool
-)
-
 func main() {
-	// parse flag & args
-	flag.BoolVar(&eiji, "eiji", false, "Search Japanese-English dictionary on Weblio.")
-	flag.BoolVar(&eiji, "e", false, "Alias for --eiji")
-	flag.Parse()
-
-	if len(flag.Args()) != 1 {
-		fmt.Println("Invalid arguments!")
+	if err := run(os.Args[1:], os.Stdout, os.Stderr); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
 
-	word := flag.Args()[0]
+func run(args []string, stdout, stderr io.Writer) error {
+	return runWithDeps(args, stdout, stderr, getWeblioDict, getWeblioEijiDict)
+}
 
-	if eiji {
-		getWeblioEijiDict(word)
-	} else {
-		getWeblioDict(word)
+type dictFunc func(word string, out io.Writer) error
+
+func runWithDeps(args []string, stdout, stderr io.Writer, dict, eijiDict dictFunc) error {
+	fs := flag.NewFlagSet("dicto", flag.ContinueOnError)
+	fs.SetOutput(stderr)
+
+	eiji := fs.Bool("eiji", false, "Search Japanese-English dictionary on Weblio.")
+	fs.BoolVar(eiji, "e", false, "Alias for --eiji")
+	fs.Usage = func() {
+		fmt.Fprintln(stderr, "Usage: dicto [--eiji|-e] <word>")
 	}
+
+	if err := fs.Parse(args); err != nil {
+		return err
+	}
+
+	if fs.NArg() != 1 {
+		fs.Usage()
+		return errors.New("invalid arguments")
+	}
+
+	word := fs.Arg(0)
+	if *eiji {
+		return eijiDict(word, stdout)
+	}
+	return dict(word, stdout)
 }
--- weblio.go
+++ weblio.go
@@ -1,104 +1,147 @@
-/**
- * This file is for providing to get web page from weblio.jp
- */
+// This file provides helpers to fetch pages from weblio.jp.
 
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/fatih/color"
 )
 
-// getWeblioDict function prints dictionary in weblio.jp.
-// Top of discription block and wikipedia-jp discription will be print.
-func getWeblioDict(word string) {
-	var str string
+const (
+	weblioBaseURL     = "https://www.weblio.jp/content/"
+	weblioEijiBaseURL = "https://ejje.weblio.jp/content/"
+)
+
+var httpClient = &http.Client{
+	Timeout: 10 * time.Second,
+}
+
+// getWeblioDict prints a dictionary entry from weblio.jp.
+// It prints the heading and the top dictionary/wikipedia excerpts.
+func getWeblioDict(word string, out io.Writer) error {
 	yellow := color.New(color.Bold, color.FgYellow).SprintFunc()
 
 	// get page from weblio.jp
-	url := "http://www.weblio.jp/content/" + word
-	doc, err := goquery.NewDocument(url)
-
+	doc, err := fetchDocument(context.Background(), weblioBaseURL+word)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+	return renderWeblioDict(doc, word, out, yellow)
+}
 
-	// trim elemets
+func renderWeblioDict(doc *goquery.Document, word string, out io.Writer, yellow func(a ...any) string) error {
+	// trim elements
 	doc.Find(".NetDicHead .midashigo > span").Empty()
 
 	// print midashigo
-	str = doc.Find(".NetDicHead .midashigo").Text()
-	if str != "" {
-		fmt.Println(yellow(str))
-	} else {
-		str = word
-		fmt.Println(yellow(str))
+	head := strings.TrimSpace(doc.Find(".NetDicHead .midashigo").Text())
+	if head == "" {
+		head = word
 	}
+	fmt.Fprintln(out, yellow(head))
 
 	// print dictionary bodies
-	doc.Find("#cont .kiji .NetDicBody > div > div > div > div").Each(func(_ int, s *goquery.Selection) {
-		str = strings.TrimSpace(s.Text())
-		if str != "" {
-			fmt.Println(str)
+	doc.Find("#cont .kiji > div").Each(func(_ int, s *goquery.Selection) {
+		text := strings.TrimSpace(s.Text())
+		if text != "" {
+			fmt.Fprintln(out, text)
 		}
 	})
 	// print Wikipedia-ja text
-	str = strings.TrimSpace(doc.Find(".kiji .Wkpja .WkpjaTs").Next().Text())
-	if str != "" {
-		fmt.Println(str)
+	wikipedia := strings.TrimSpace(doc.Find(".kiji .Wkpja .WkpjaTs").Next().Text())
+	if wikipedia != "" {
+		fmt.Fprintln(out, wikipedia)
 	}
+	return nil
 }
 
-// getWeblioEijiDict function prints dictionary in ejje.weblio.jp.
-// summary and example writing will be print.
-func getWeblioEijiDict(word string) {
-	var str string
+// getWeblioEijiDict prints a dictionary entry from ejje.weblio.jp.
+// It prints the summary and example sentences.
+func getWeblioEijiDict(word string, out io.Writer) error {
 	green := color.New(color.Bold, color.FgGreen).SprintFunc()
 	yellow := color.New(color.FgYellow).SprintFunc()
 	bold := color.New(color.Bold).SprintFunc()
 
 	// get page from ejje.weblio.jp
-	url := "http://ejje.weblio.jp/content/" + word
-	doc, _ := goquery.NewDocument(url)
-
-	// print query
-	str = doc.Find("#h1Query").Text()
-	if str != "" {
-		fmt.Println(green(str))
-	} else {
-		str = word
-		fmt.Println(green(str))
+	doc, err := fetchDocument(context.Background(), weblioEijiBaseURL+word)
+	if err != nil {
+		return err
 	}
+	return renderWeblioEijiDict(doc, word, out, green, yellow, bold)
+}
+
+func renderWeblioEijiDict(
+	doc *goquery.Document,
+	word string,
+	out io.Writer,
+	green func(a ...any) string,
+	yellow func(a ...any) string,
+	bold func(a ...any) string,
+) error {
+	// print query
+	query := strings.TrimSpace(doc.Find("#h1Query").Text())
+	if query == "" {
+		query = word
+	}
+	fmt.Fprintln(out, green(query))
 
 	// print summary
-	summary := doc.Find(".summaryM table tbody tr td.content-explanation").Text()
-	fmt.Println(summary)
+	summary := strings.TrimSpace(doc.Find(".summaryM table tbody tr td.content-explanation").Text())
+	fmt.Fprintln(out, summary)
 
 	// print example documents
-	fmt.Print(bold("\n[Examples]\n"))
+	fmt.Fprint(out, bold("\n[Examples]\n"))
 	doc.Find(".qotC").Each(func(_ int, s *goquery.Selection) {
 		s.Find(".qotCE > span").Empty()
 		s.Find(".qotCJE > span").Empty()
 		s.Find(".qotCJJ > span").Empty()
-		exampleHead := s.Find(".qotCE").Text()
-		exampleTail := s.Find(".qotCJ").Text()
+		exampleHead := strings.TrimSpace(s.Find(".qotCE").Text())
+		exampleTail := strings.TrimSpace(s.Find(".qotCJ").Text())
 		if exampleHead == "" {
-			exampleHead = s.Find(".qotCJE").Text()
-			exampleTail = s.Find(".qotCJJ").Text()
+			exampleHead = strings.TrimSpace(s.Find(".qotCJE").Text())
+			exampleTail = strings.TrimSpace(s.Find(".qotCJJ").Text())
 		}
 		if exampleHead != "" {
-			fmt.Println(yellow(exampleHead))
-			fmt.Println(exampleTail)
+			fmt.Fprintln(out, yellow(exampleHead))
+			fmt.Fprintln(out, exampleTail)
 		}
 	})
 
 	// print more link
 	moreExample, _ := doc.Find(".hlt_SNTCE .kiji > a").Attr("href")
 	ul := color.New(color.Underline).SprintFunc()
-	fmt.Println("\n", ul(moreExample))
-	fmt.Println("")
+	fmt.Fprintln(out, "\n", ul(moreExample))
+	fmt.Fprintln(out, "")
+	return nil
+}
+
+func fetchDocument(ctx context.Context, url string) (*goquery.Document, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if closeErr := resp.Body.Close(); closeErr != nil {
+			log.Printf("failed to close response body: %v", closeErr)
+		}
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status: %s", resp.Status)
+	}
+
+	return goquery.NewDocumentFromReader(resp.Body)
 }
